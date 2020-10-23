fa81f7906bfb9d07487d2714120a7954c564f5d5
hawkup <spiritsquare@gmail.com>
2020-10-23 14:49:37 +0000

    Load test check status response

--- _load_test/k6/script.js
+++ _load_test/k6/script.js
@@ -1,5 +1,9 @@
+import { check } from 'k6';
 import http from 'k6/http'
 
 export default function() {
-    http.get('http://localhost:8080');
+    let res = http.get('http://localhost:8080');
+    check(res, {
+        'is status 200': (r) => r.status === 200,
+    });
 }
